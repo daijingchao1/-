d725d0c97794a8d1676ad80fce9fdc8e0887001a
djc1 <djc1@digitalchina.com>
2021-03-03 13:45:37 +0800

    改变

--- readXml.go
+++ readXml.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"io/ioutil")
+	"io/ioutil"
+	"os"
+)
 
+//SConfig 指定数据类型
+type SConfig struct {
+	XMLName      xml.Name   `xml:"config"`     // 指定最外层的标签为config
+	SMTPServer   string     `xml:"smtpServer"` //SmtpServer 读取smtpServer配置项，并将结果保存到SmtpServer变量中
+	SMTPPort     int        `xml:"smtpPort"`
+	Sender       string     `xml:"sender"`
+	SenderPasswd string     `xml:"senderPasswd"`
+	Receivers    SReceivers `xml:"receivers"` // 读取receivers标签下的内容，以结构方式获取
+}
+
+//SReceivers 指定数据类型
+type SReceivers struct {
+	Flag string   `xml:"flag,attr"` // 读取flag属性
+	User []string `xml:"user"`      // 读取user数组
+}
 
 func main() {
-	content,_:=ioutil.ReadFile("aaa.xml")
-	fmt.Printf("content:%s\n",content)
+	file, err := os.Open("aaa.xml") // For read access.
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	v := SConfig{}
+	err = xml.Unmarshal(data, &v)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	fmt.Println(v)
 }
